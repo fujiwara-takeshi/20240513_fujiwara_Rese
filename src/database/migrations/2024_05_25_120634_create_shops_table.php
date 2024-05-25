ed80f3fa2d54e0c6e714ef7e536b6f937784556a
fujiwara takeshi <tfdxvjiyre75528@gmail.com>
2024-05-25 16:23:35 +0900

    元のデータベースを作成

---
+++ src/database/migrations/2024_05_25_120634_create_shops_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('area_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('genre_id')->constrained()->cascadeOnDelete();
+            $table->string('name', 50);
+            $table->string('detail');
+            $table->string('image_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
